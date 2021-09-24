1a09099d4c36813b18079e36746418ff29c03676
Sipe <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-09-24 12:40:39 +0200

    feat(collectoins_screen and descriptioncar_widget): add the skeleton of the card description for the collection section

---
+++ lib/assets/widgets/descriptioncard_widget.dart
@@ -0,0 +1,42 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class DescriptionCardWidget extends StatelessWidget {
+  const DescriptionCardWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:  Card(
+        child:  SizedBox(
+          height: 100,
+          child:  Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: <Widget>[
+              Text('Hello World'),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('How are you?'),
+
+                  Text('How are you?'),
+                ],
+              ),
+
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text('How are you?'),
+                  Text('How are you?'),
+                ],
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+
+  }}
