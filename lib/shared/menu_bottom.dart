8f8750db449927cd12f27d3845888d1febb290af
Sipe <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-09-25 20:34:21 +0200

    :children_crossing: #5 ajust the menu in particular the active and the non active icon when we navigate

--- lib/shared/menu_bottom.dart
+++ lib/shared/menu_bottom.dart
@@ -5,17 +5,20 @@ import 'package:board_game_app/pages/wishlist_screen.dart';
 import 'package:flutter/material.dart';
 
 class MenuBottom extends StatelessWidget {
+
   const MenuBottom({
     Key? key,
+    page
   }) : super(key: key);
 
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static String activePage = 'Home';
+  static int _selectedIndex = 0;
+
 
   @override
   Widget build(BuildContext context) {
-    int _selectedIndex = 0;
-
     return BottomNavigationBar(
       onTap: (int index){
         switch (index){
@@ -24,51 +27,62 @@ class MenuBottom extends StatelessWidget {
                 builder: (context) =>
                  const HomeScreen())
             );
+            _selectedIndex = index;
             break;
           case 1:
             Navigator.push(context, MaterialPageRoute(
                 builder: (context) =>
                     const CollectionsScreen())
             );
+            _selectedIndex = index;
             break;
           case 2:
             Navigator.push(context, MaterialPageRoute(
                 builder: (context) =>
                     const WishListScreen())
             );
+            _selectedIndex = index;
             break;
           case 3:
             Navigator.push(context, MaterialPageRoute(
                 builder: (context) =>
                     const SettingsScreen())
             );
-          break;
+            _selectedIndex = index;
+            break;
         }
+
       },
-      items: const [
+      items:  [
         BottomNavigationBarItem(
-          icon: Icon(Icons.home),
+          icon: Icon(Icons.home, color: isPageActive(_selectedIndex,0)),
           label: 'Home',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.view_column_outlined),
+          icon: Icon(Icons.view_column_outlined, color: isPageActive(_selectedIndex,1)),
           label: 'Collections',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.favorite),
+          icon: Icon(Icons.favorite, color: isPageActive(_selectedIndex,2)),
           label: 'WishList',
         ),
         BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
+          icon: Icon(Icons.settings, color: isPageActive(_selectedIndex,3)),
           label: 'Settings',
         ),
       ],
-      backgroundColor: Colors.black12,
-      unselectedItemColor: Colors.lightBlue,
       currentIndex: _selectedIndex,
-      selectedItemColor: Colors.amber[800],
+      backgroundColor: Colors.black12,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      unselectedItemColor: Colors.grey,
+      selectedItemColor: Colors.blueAccent,
+
     );
+
   }
 
-
+  Color isPageActive(int activePage, int labelPage){
+    return activePage==labelPage ? Colors.blueAccent : Colors.grey;
+  }
 }
