6da052764b5d2932b60a458302c07760da45c54b
Pierre <pierre.darcas@hotmail.fr>
2021-09-27 09:01:13 +0200

    :construction: ApiSearch class

---
+++ lib/apiSearch.dart
@@ -0,0 +1,17 @@
+
+import 'package:flutter/cupertino.dart';
+
+class  ApiSearch extends StatefulWidget {
+  const ApiSearch({Key? key}) : super(key: key);
+
+  @override
+  _ApiSearchState createState() => _ApiSearchState();
+
+}
+
+class _ApiSearchState extends State<ApiSearch> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
