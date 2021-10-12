ad77195243531de22faec8fcadd16ac234ac2f2f
Julien Dudek <julien.dudek@gmail.com>
2021-10-13 00:09:59 +0200

    :white_check_mark: add Test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,4 +1,5 @@
 import 'package:board_game_app/pages/auth_screen.dart';
+import 'package:board_game_app/pages/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
@@ -22,4 +23,23 @@ void main() {
     //Button
     expect(find.byType(ElevatedButton), findsOneWidget);
   });
+
+  testWidgets('Bottom Navigation Bar Tests', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      const MaterialApp(
+        home: HomeScreen(),
+    ));
+
+    // Simply verify Icons and text
+    expect(find.text('Home'), findsWidgets);
+    expect(find.byIcon(Icons.home), findsOneWidget);
+    expect(find.text('WishList'), findsOneWidget);
+    expect(find.byIcon(Icons.favorite), findsOneWidget);
+    expect(find.text('Settings'), findsOneWidget);
+    expect(find.byIcon(Icons.settings), findsOneWidget);
+    expect(find.text('Collections'), findsOneWidget);
+    expect(find.byIcon(Icons.view_column_outlined), findsOneWidget);
+  });
+
 }
