6cdf68aea460e4235dd41d7c33394f83a622eced
Julien Dudek <julien.dudek@gmail.com>
2021-10-12 22:38:25 +0200

    ✅ Tests (For CI ;) )

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,43 +1,25 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:board_game_app/pages/home_screen.dart';
-import 'package:board_game_app/pages/settings_screen.dart';
-import 'package:board_game_app/pages/wishlist_screen.dart';
+import 'package:board_game_app/pages/auth_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:board_game_app/pages/collections_screen.dart';
+
 
 void main() {
 
-  testWidgets('Bottom Navigation Bar Tests', (WidgetTester tester) async {
+  testWidgets('AuthScreen on Start', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(
-       MaterialApp(
-        routes: {
-          '/': (context) => const HomeScreen(),
-          '/Collections': (context) => const CollectionsScreen(),
-          '/WishList': (context) => const WishListScreen(),
-          '/Settings': (context) => const SettingsScreen(),
-        },
-        initialRoute: '/',
-
+       const MaterialApp(
+        home:AuthScreen(),
       ),
     );
 
-    await tester.tap(find.byIcon(Icons.settings));
-    // Simply verify Icons and text
-    expect(find.text('Home'), findsWidgets);
-    expect(find.byIcon(Icons.home), findsOneWidget);
-    expect(find.text('WishList'), findsOneWidget);
-    expect(find.byIcon(Icons.favorite), findsOneWidget);
-    expect(find.text('Settings'), findsOneWidget);
-    expect(find.byIcon(Icons.settings), findsOneWidget);
-    expect(find.text('Collections'), findsOneWidget);
-    expect(find.byIcon(Icons.view_column_outlined), findsOneWidget);
+    // Text on Page
+    expect(find.text('Login'), findsWidgets);
+    expect(find.text('Welcome back ! Login with your credentials'), findsOneWidget);
+    expect(find.text('Signup'), findsOneWidget);
+    //Textfield
+    expect(find.byType(TextField), findsNWidgets(2));
+    //Button
+    expect(find.byType(ElevatedButton), findsOneWidget);
   });
 }
