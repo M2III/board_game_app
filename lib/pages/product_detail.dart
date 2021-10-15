08ba77d430c3c4266d9100113e58f4f56ec7edb7
Pierre <pierre.darcas@hotmail.fr>
2021-10-15 08:54:51 +0200

    🚧 redirection to product detail OK, card to work on

--- lib/pages/product_detail.dart
+++ lib/pages/product_detail.dart
@@ -1,5 +1,6 @@
 import 'package:board_game_app/models/game.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ProductDetail extends StatefulWidget {
   const ProductDetail({Key? key, required this.game}) : super(key: key);
@@ -13,8 +14,60 @@ class ProductDetail extends StatefulWidget {
 class _ProductDetailState extends State<ProductDetail> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text(widget.game.name.toString()),
+    return Expanded(
+      child: DefaultTabController(
+        length: 1,
+        child: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Center(
+                child : Image(
+                  fit: BoxFit.cover,
+                  width: 40,
+                  height: 40,
+                  image: NetworkImage(widget.game.imageUrl ?? ""),)
+              ),
+              Center(
+                child: Text(
+                  widget.game.name.toString()
+                ),
+              ),
+              Center(
+                child: Row(
+                  children: [
+                    IconButton(
+                        onPressed: (){debugPrint("tapped");},
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.green,
+                          size: 40,
+                        )),
+                    IconButton(
+                        onPressed: (){debugPrint("tapped");},
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.red,
+                          size: 40,
+                        )),
+                    IconButton(
+                        onPressed: (){debugPrint("tapped");},
+                        icon: Icon(
+                          Icons.add,
+                          color: Colors.blue,
+                          size: 40,
+                        )),
+                  ],
+                ),
+              ),
+              Center(
+                child : Text(
+                  widget.game.description.toString()
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
