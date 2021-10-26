25ab8121548cd07aecb9e489a9165d2c31925493
Julien Dudek <julien.dudek@gmail.com>
2021-10-27 00:39:10 +0200

    🚨 for lint prefere // vs **/ for comment

--- lib/widgets/to_html.dart
+++ lib/widgets/to_html.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 
-/**
- * Classe Utilitaire permettant d'intépreter l'HTML récupéré par l'API
- */
+/// Classe Utilitaire permettant d'intépreter l'HTML récupéré par l'API
 class ToHtml extends StatelessWidget {
   final String html;
   const ToHtml({Key? key,var data, required this.html}) : super(key: key);
