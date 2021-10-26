73c49fcc9dee8a8cd57593f190337cb9c3a294c9
Morgan <morgan.lombard@protonmail.com>
2021-10-26 11:57:14 +0200

    CustTextField and CollectionScreen refactoring

--- lib/pages/collections_screen.dart
+++ lib/pages/collections_screen.dart
@@ -2,50 +2,18 @@ import 'package:board_game_app/widgets/menu_bottom.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import '../widgets/customtextfield_widget.dart';
+import '../widgets/search_bar.dart';
 
 class CollectionsScreen extends StatelessWidget {
   const CollectionsScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //int _selectedIndex = 0;
-    TextEditingController textController = TextEditingController();
-    const TextStyle hintStyle = TextStyle(color: Colors.black26,);
-
-    String hintText = "Your text";
-    BorderSide borderSide = const BorderSide(color: Colors.transparent,);
-    BorderRadius borderRadius = BorderRadius.circular(10.0);
-    BorderRadius focusedBorderRadius = BorderRadius.circular(25.7);
-    TextStyle labelStyle = const TextStyle(color: Color.fromARGB(174, 182, 191, 1), fontSize: 15, fontStyle: FontStyle.normal);
-    Color fillColor = const Color.fromRGBO(255, 255, 255, 1);
-
     return MaterialApp(
       home: Scaffold(
-
+        body: Scaffold(body: SearchBar()),
         bottomNavigationBar: const MenuBottom(),
-        body: Scaffold(
-            body: Stack(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(10),
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 50),
-                        CustTextField(textController: textController, hintText: hintText, hintStyle: hintStyle, labelStyle: labelStyle, fillColor: fillColor, borderSide: borderSide, focusedBorderRadius: focusedBorderRadius, borderRadius: borderRadius),
-                        const SizedBox(height: 25),
-                        //DescriptionCardWidget(game: null,),
-                      ],
-                    ),
-                  )
-                ]
-            )
-        ),
       ),
     );
   }
 }
-
---
+++ lib/widgets/search_bar.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      Container(
+        padding: const EdgeInsets.all(10),
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          children: [
+            const SizedBox(height: 50),
+            getSearchField(),
+            const SizedBox(height: 25),
+            //DescriptionCardWidget(game: null,),
+          ],
+        ),
+      )
+    ]);
+  }
+
+  Widget getSearchField() {
+    return TextField(
+      controller: textController,
+      decoration: InputDecoration(
+          hintText: "Search a game...",
+          hintStyle: const TextStyle(
+            color: Colors.black26,
+          ),
+          labelStyle: const TextStyle(
+              color: Color.fromARGB(174, 182, 191, 1),
+              fontSize: 15,
+              fontStyle: FontStyle.normal),
+          filled: true,
+          fillColor: const Color.fromRGBO(255, 255, 255, 1),
+          prefixIcon: const Icon(Icons.email),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+            borderRadius: BorderRadius.circular(25.7),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.transparent,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
+          )),
+    );
+  }
+}
