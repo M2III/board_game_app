3357b6bcda76032e388c48a881f6da151ff23e31
Morgan <morgan.lombard@protonmail.com>
2021-11-09 17:02:43 +0100

    update repo without async await

--- lib/data/repositories/game_repository.dart
+++ lib/data/repositories/game_repository.dart
@@ -1,27 +1,21 @@
 import 'package:board_game_app/data/models/all_response_games.dart';
 import 'package:board_game_app/data/providers/remote/game_api_provider.dart';
 
-
-class GameRepository{
-  static final GameRepository _singleton=
-      GameRepository._internal();
-  factory GameRepository()=> _singleton;
+class GameRepository {
+  static final GameRepository _singleton = GameRepository._internal();
+  factory GameRepository() => _singleton;
 
   GameRepository._internal();
 
-
-  Future<AllResponseGames> getGamesOrderByRank() async {
+  Future<AllResponseGames> getGamesOrderByRank() {
     return GameApiProvider().getGamesOrderByRank();
   }
 
-  Future<AllResponseGames>
-  getPopularKickstartersOrderByTrendingRank() async {
-    
+  Future<AllResponseGames> getPopularKickstartersOrderByTrendingRank() {
     return GameApiProvider().getPopularKickstartersOrderByTrendingRank();
   }
 
-  Future<AllResponseGames> searchGamesByInput(String input) async {
+  Future<AllResponseGames> searchGamesByInput(String input) {
     return GameApiProvider().searchGamesByInput(input);
   }
-  
 }
