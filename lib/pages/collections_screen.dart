84b3e290710042220ce629b9b79ee9bcbf1f3956
tchawou-daniel <daniel-jordan.sipe-tchawou@lacatholille.fr>
2021-12-21 01:20:37 +0100

    :recycle: clean the code

--- lib/pages/collections_screen.dart
+++ lib/pages/collections_screen.dart
@@ -1,6 +1,5 @@
 import 'package:board_game_app/resources/widgets/menu_bottom.dart';
 import 'package:board_game_app/resources/widgets/search_bar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class CollectionsScreen extends StatelessWidget {
