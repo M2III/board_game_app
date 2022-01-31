f6cbc589d441ac13754c94f8755fca5e7afe1ab1
Morgan <morgan.lombard@protonmail.com>
2022-01-31 10:38:15 +0100

    Clean WishlistsBloc

--- lib/pages/WishList/wishlists_bloc.dart
+++ lib/pages/WishList/wishlists_bloc.dart
@@ -2,35 +2,24 @@ import 'package:board_game_app/data/models/all_response_games.dart';
 import 'package:board_game_app/data/models/collections.dart';
 import 'package:board_game_app/data/repositories/game_repository.dart';
 
-
 class WishlistsBloc {
   final _repository = GameRepository();
 
-  Collections? showGameDetails(String input)  {
-    var collection = _repository.getCollection(input);
-    return collection;
+  Collections? showGameDetails(String input) {
+    return _repository.getCollection(input);
   }
 
   List<Collections>? getAllCollection() {
-    final _collections = _repository.getAll();
-    List<Collections> temp =[];
-    for(var i=0; i < _collections!.length; i++){
-      if(_collections.elementAt(i).wish){
-        temp.add(_collections.elementAt(i))  ;
-      }
-    }
-
-    return temp;
+    return _repository.getAll()!.where((element) => element.wish).toList();
   }
 
   Future<void>? deleteCollections(key) {
     return _repository.deleteACollection(key);
   }
 
-  Future<AllResponseGames> getDetailWishListGame(String idGame)  {
-    var game =  _repository.getDetailGame(idGame);
-    return game;
+  Future<AllResponseGames> getDetailWishListGame(String idGame) {
+    return _repository.getDetailGame(idGame);
   }
-
 }
+
 final wishlistsBloc = WishlistsBloc();
